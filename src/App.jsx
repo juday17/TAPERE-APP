d0f10acd2772b23f05ca1a0c8b71cd87833b4bbb
juday17 <judytapere259@gmail.com>
2024-07-12 17:11:43 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Judy Tapere</h1>
+      <h2>NO PARTNER</h2>
+    </div>
+  );
+}
+
+export default App;
